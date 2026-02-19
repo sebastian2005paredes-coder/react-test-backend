ba924da2e57f03b35ed6e90d578bd5a503cc15af
sebastian123gonzalo <sebastian2005paredes@gmail.com>
2026-02-19 13:12:09 -0500

    feat: db.json updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
-require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: "maglev.proxy.rlwy.net",
+    user: "root",
+    password: "oQNwBWAuzOEwfIdXMlOCzeDQOZjVzPYc",
+    database: "railway",
+    port: 10245
 });
 
 connection.connect((err) => {
