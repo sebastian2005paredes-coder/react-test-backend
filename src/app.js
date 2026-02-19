d2c21a598478b684caad8461b4494017d2be830f
sebastian123gonzalo <sebastian2005paredes@gmail.com>
2026-02-19 12:44:39 -0500

    feat: CORS methods updated

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const orderProductsRoutes = require("./routes/orderProducts.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.use("/products", productsRoutes);
