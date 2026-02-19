d9c0ab99ce4d3ba2a5b939a8d141adad603135f6
sebastian123gonzalo <sebastian2005paredes@gmail.com>
2026-02-19 13:37:13 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,8 +17,13 @@ app.use(cors({
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    
     next();
 });
 
